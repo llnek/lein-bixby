e6b1371a00518cf72ec10608928fe0af3a9e514a
Kenneth Leung <ccllnekk@gmail.com>
2017-01-12 02:52:09 -0800

    kl

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :license {:url "http://www.eclipse.org/legal/epl-v10.html"
             :name "Eclipse Public License"}
 
-  :scm "https://github.com/llnek/lein-wabbit.git"
+  ;;:scm "https://github.com/llnek/lein-wabbit.git"
   :url "https://github.com/llnek/lein-wabbit"
 
   ;;{:deploy-repositories [["releases" :clojars]]}
